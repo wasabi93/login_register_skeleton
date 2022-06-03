e1b5962d633e253002158c3b78b530c16d55a718
Lê Thọ <85331765+wasabi93@users.noreply.github.com>
2022-06-03 12:47:06 +0700

    log test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,8 @@ const Status = dynamic(() => import('./status'));
 const Home: NextPage = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { data: session } = useSession();
 
+  console.log('session', session);
+
   return (
     <div className={styles.container}>
       <Head>
